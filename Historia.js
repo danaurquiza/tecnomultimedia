8196b3428f267823abfc55aab17380bcd66c8b76
Dana <83373726+danaurquiza@users.noreply.github.com>
2023-11-03 11:57:15 -0300

    Add files via upload

---
+++ Historia.js
@@ -0,0 +1,34 @@
+class Historia(){
+
+constructor(){
+this.crearMinijuego();
+
+}
+
+dibujar(){
+this.Minijuego.dibujar();
+
+}
+
+configuracionDeTexto(){
+
+}
+
+botones(){
+
+}
+  
+estoyDentrodeBotonCircular(){
+
+}
+
+estoyDentrodeBotonCuadrado(){
+
+}
+
+crearMinijuego(){
+this.Minijuego = new Minijuego()
+
+}
+
+}
---
+++ tp6.js
@@ -0,0 +1,12 @@
+let objHistoria;
+
+function setup() {
+createCanvas(600,600);
+objHistoria = new Historia();
+
+}
+
+
+function draw() {
+
+}
