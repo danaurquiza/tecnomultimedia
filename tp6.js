9e83c8800998de753e344f82ba5df284f5e50827
Inés Rozankovic <103336169+inerozan@users.noreply.github.com>
2023-11-13 12:51:56 -0300

    Add files via upload

--- tp6.js
+++ tp6.js
@@ -1,6 +1,7 @@
 let objHistoria;
 let fondo = [];
 let texto = [];
+let bot = [];
 
 function setup() {
 createCanvas(600,600);
@@ -19,12 +20,15 @@ objHistoria.dibujar(fondo,texto);
 
 function preload(){
   
-  for( let i = 0 ; i < 17 ; i++ ){
+  for( let i = 0 ; i < 21 ; i++ ){
     fondo[i] = loadImage('assets/rata'+ i +'.png');
   }
   
-  for( let x = 0 ; x < 20 ; x++ ){
     texto = loadStrings('assets/texto.txt');
-  }
   
+    bot =loadStrings('assets/bot.txt'); 
+}
+
+function mousePressed(){
+objHistoria.botonPresionado(mousePressed); 
 }
