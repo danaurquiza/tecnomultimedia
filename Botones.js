559fee47257d17bd91bec9226dfd65a5884b0fb3
Dana <83373726+danaurquiza@users.noreply.github.com>
2023-12-06 20:27:34 -0300

    Add files via upload

--- Botones.js
+++ Botones.js
@@ -1,43 +1,14 @@
-class Botones {
-
-  constructor(textoBoton) {
-    this.texto=textoBoton;
-
-    this.botonCircular = new botonCircular(this.texto, this.estoyDentrodeBotonCircular, this.textoBotones);
-    this.botonTransparente = new botonCircularTransparente(this.estoyDentrodeBotonCircular);
-    this.botonCuadrado= new botonCuadradoPuerta(this.estoyDentrodeBotonCuadrado, this.textoBotones, this.texto);
-  }
-
-  dibujar() {
-  }
-
-  textoBotones() {
-    fill(0);
-    textSize(17);
-    textAlign(CENTER);
-  }
-
-  estoyDentrodeBotonCircular( posX, posY, r ) {
-    return dist(mouseX, mouseY, posX, posY) <= r;
-  }
-
-  estoyDentrodeBotonCuadrado(x, x1, y, y1) { //con parametros con return
-    return mouseX> x && mouseX< x1 && mouseY> y && mouseY< y1;
-  }
-}
-
 //---------------------------------------------------------------------Botones Circulares
 class  botonCircular {
 
-  constructor(textoBoton, eDBC, tBs) {
-    this.parametroCircular=eDBC;
-    this.textoBoton=tBs;
+  constructor(textoBoton) {
+
     this.texto=textoBoton;
   }
 
   dibujar(posX, posY, r, col1, col2, i, xTe, yTe) {
 
-    if (this.parametroCircular(posX, posY, r/2) ) {
+    if (this.estoyDentrodeBotonCircular(posX, posY, r/2) ) {
       fill(col1);
     } else {
       fill(col2); //verde
@@ -45,20 +16,26 @@ class  botonCircular {
 
     noStroke();
     circle(posX, posY, r);
-    this.textoBoton();
+    fill(0);
+    textSize(17);
+    textAlign(CENTER);
+  
     text(this.texto[i], xTe, yTe);
+  }
+
+  estoyDentrodeBotonCircular( posX, posY, r ) {
+    return dist(mouseX, mouseY, posX, posY) <= r;
   }
 }
 //--------------------------------------------------------------------Botones Transparentes
 class botonCircularTransparente {
-  constructor(eDBC) {
-    this.parametroCircular=eDBC;
+  constructor() {
   }
   dibujar(posX, posY, r) {
 
 
     //BOTON CREDITO REDONDO
-    if (this.parametroCircular(posX, posY, r/2) ) {
+    if (this.estoyDentrodeBotonCircular(posX, posY, r/2) ) {
       fill(227, 255, 100, 100);
     } else {
       noFill(); //verde
@@ -67,16 +44,20 @@ class botonCircularTransparente {
     noStroke();
     circle(posX, posY, r);
   }
+
+  estoyDentrodeBotonCircular( posX, posY, r ) {
+    return dist(mouseX, mouseY, posX, posY) <= r;
+  }
 }
 //------------------------------------------------------------------Boton Puerta
 class  botonCuadradoPuerta {
-  constructor(eDbC, tBs) {
-    this.estoyDentro=eDbC;
-    this.textoBotones=tBs;
+  constructor() {
+
   }
+  
   dibujar(x, x1, y, y1, ancho, alto ) {
     //BOTON AVANZAR PUERTA CUADRADO
-    if (this.estoyDentro( x, x1, y, y1 )) {
+    if (this.estoyDentrodeBotonCuadrado( x, x1, y, y1 )) {
 
       fill( 255, 0, 0, 150 );
     } else {
@@ -85,7 +66,13 @@ class  botonCuadradoPuerta {
     }
 
     rect(x, y, ancho, alto);
-    this.textoBotones();
+    fill(0);
+    textSize(17);
+    textAlign(CENTER);
     text("Inicio", 370, 272);
+  }
+
+  estoyDentrodeBotonCuadrado(x, x1, y, y1) { //con parametros con return
+    return mouseX> x && mouseX< x1 && mouseY> y && mouseY< y1;
   }
 }
--- tpfinal.js
+++ tpfinal.js
@@ -3,11 +3,11 @@
  //Legajos 91470/0 - 81867/3
 
 
+//VARIABLES GLOBALES
 let objPantallas;
 let fondo = [];
 let fondoMinijuego = [];
 let texto = [];
-
 let titulo;
 let regular;
 let canasta, plata;
@@ -15,10 +15,9 @@ let canasta, plata;
 function setup() {
 createCanvas(600,600);
 
-objPantallas = new Pantallas(fondo, texto, titulo, regular, canasta, plata,fondoMinijuego);
+objPantallas = new Pantallas(fondo, texto, titulo, regular, canasta, plata, fondoMinijuego);  //paso variables globales al objeto para mantener orden
 
 }
-
 
 function draw() {
 background(200,200,0);
@@ -29,28 +28,24 @@ objPantallas.dibujar();
 
 function preload(){
   
-   for( let i = 0 ; i < 22 ; i++ ){
+   for( let i = 0 ; i < 22 ; i++ ){ //fondo de la historia
     fondo[i] = loadImage('assets/rata'+ i +'.png');
   }
-  for( let i = 0 ; i < 2 ; i++ ){
+  for( let i = 0 ; i < 2 ; i++ ){ //fondo de minijuego
     fondoMinijuego[i] = loadImage('assets/fondo'+ i +'.png');
   }
-  
   canasta = loadImage ("assets/canasta.png");
   plata = loadImage ("assets/plata.png");
   
    texto = loadStrings('assets/texto.txt');
-  
-
-    
    titulo = loadFont('assets/titulo.ttf');
-   
    regular = loadFont('assets/regular.ttf');
 }
 
 function mousePressed(){
   objPantallas.botonPresionado(mousePressed); 
 }
+
 function keyPressed() {
  objPantallas.Minijuego.teclaPresionada(keyCode);
 }
