954bfdbe3f3a24a0d28cd96ca3f27bde9383a184
Dana <83373726+danaurquiza@users.noreply.github.com>
2024-06-26 23:45:24 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-//https://github.com/danaurquiza/tecnomultimedia/tree/tp1tecno2
+//https://youtu.be/rntG5uMsCZ8
 const model_url = 'https://cdn.jsdelivr.net/gh/ml5js/ml5-data-and-models/models/pitch-detection/crepe/';
 
 let obra;  
